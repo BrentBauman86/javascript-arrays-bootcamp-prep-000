6a84c6688efe91b128bfa4969cc63fa3749d8017
Brent Bauman <brentbauman86@gmail.com>
2018-03-11 22:24:46 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,37 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array, element){
+  return [element, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  return [array.unshift, element]
+}
+
+function addElementToEndOfArray(array, element){
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  return [array.push, element]
+}
+
+function accessElementInArray(1,2,3), 2){
+  return [3]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  return [array.shift()]
+}
+
+function removeElementFromBeginningOfArray(array){
+  return [array.slice(1)]
+}
+
+function destructivelyAddElementToEndOfArray(array){
+  return [array.pop()]
+}
+
+function removeElementFromEndOfArray(array){
+  return [array.length - 1]
+}
